e23e2867717170aa38e59ebd0423a23b6f113ff8
Blessing Ekundayo <blessochampion@gmail.com>
2017-04-15 23:24:31 +0100

    Activitities created

---
+++ app/src/main/java/com/popularmovies/popularmovies/Config.java
@@ -0,0 +1,12 @@
+package com.popularmovies.popularmovies;
+
+/**
+ * Created by blessochampion on 4/15/17.
+ */
+
+public class Config
+{
+    /*Change the API_KEY to the one given to your on the moviedb.org website*/
+    public static final String MOVIE_DB_API_KEY = "YOUR API KEY";
+
+}
---
+++ app/src/main/java/com/popularmovies/popularmovies/activities/PopularMoviesActivity.java
@@ -0,0 +1,15 @@
+package com.popularmovies.popularmovies.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.popularmovies.popularmovies.R;
+
+public class PopularMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movies);
+    }
+}
