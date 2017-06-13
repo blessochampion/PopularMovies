858968cfa07e29bd253f8a98873bc6c2be097ea1
Blessing Ekundayo <Blessing.Ekundayo@Interswitchng.com>
2017-06-13 17:47:48 +0100

    remove AsyncTaskLoader for Reviews

--- app/src/main/java/com/popularmovies/popularmovies/fragments/ReviewsFragment.java
+++ app/src/main/java/com/popularmovies/popularmovies/fragments/ReviewsFragment.java
@@ -7,18 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.popularmovies.popularmovies.R;
 
 /**
  * Created by Blessing.Ekundayo on 6/13/2017.
  */
 
-public class ReviewsFragment extends Fragment
-{
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_reviews, container, false);
-        return rootView;
+public class ReviewsFragment extends Fragment {
+
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_reviews, container, false);
+            makeNetworkCall();
+            return rootView;
+        }
+
+    private void makeNetworkCall() {
+
     }
+
+
 }
---
+++ app/src/main/java/com/popularmovies/popularmovies/models/Review.java
@@ -0,0 +1,46 @@
+package com.popularmovies.popularmovies.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Blessing.Ekundayo on 6/13/2017.
+ */
+
+public class Review implements Parcelable{
+    private String author;
+    private String content;
+
+    public Review(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    protected Review(Parcel in) {
+        author = in.readString();
+        content = in.readString();
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(author);
+        dest.writeString(content);
+    }
+}
